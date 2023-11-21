caff0bd93e788b3efe987ba7083de0d19e9ffb4b
mislavcupic <mislav.cupic@gmail.com>
2023-11-21 17:52:28 +0100

    Mislav promjene

--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -44,6 +44,8 @@ function Navigation() {
             <NavDropdown.Item href="/about">O nama</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="/about/introductionspeech">Pozdravni govor</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="/about/pozvanipredavaci">Pozvani predavači</NavDropdown.Item>
           </NavDropdown>
           {/* Add more Nav.Link or NavDropdown items as needed */}
           <NavDropdown title="Kotizacije i smještaj" id="collasible-nav-dropdown">
